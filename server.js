c83c1ccd95dd04216361e6dfd543cebc3ccd7871
oivinnie <vinipeix42@gmail.com>
2026-07-07 15:05:28 -0300

    incluindo opção pra não considerar timeout do zap

--- server.js
+++ server.js
@@ -1691,6 +1691,7 @@ function initWhatsApp(schoolHash, schoolConfig) {
         },
         webVersion: '2.3000.1042751833-alpha',
         deviceName: `Bot ${schoolConfig.nome_fantasia || 'DKSoft'}`,
+        authTimeoutMs: 0,
         puppeteer: {
             headless: true,
             executablePath: process.env.PUPPETEER_EXECUTABLE_PATH || undefined,
